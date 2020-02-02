f82c80210960cef6f0f170077844bc0afe98522a
Gabriel Morandim <augustomorandim@gmail.com>
2020-02-02 18:22:42 -0300

    add providers and files controllers

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/user';
 import AuthController from './app/controllers/auth';
+import FileController from './app/controllers/file-controller';
+import ProviderController from './app/controllers/provider-controller';
 
 import authMiddlewares from './app/middlewares/auth';
 
@@ -17,7 +19,8 @@ routes.post('/users', UserController.store);
 routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), (req, res) => res.json({ ok: true }))
+routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 
 export default routes;
