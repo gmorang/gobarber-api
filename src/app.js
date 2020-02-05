6c85b2188dc009657f0e87ce77c844a1d3ecad6c
Gabriel Morandim <augustomorandim@gmail.com>
2020-02-04 21:46:04 -0300

    error handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
+
+import sentryConfig from './config/sentry';
+
 
 import routes from './routes';
 
@@ -9,17 +15,30 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.execptionhandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use('/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  execptionhandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors)
+    })
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://223e750adda64f37807bccae16a94147@sentry.io/2252440'
+}
