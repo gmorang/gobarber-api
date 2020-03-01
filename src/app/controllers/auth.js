940fff283df9bb044b3da158e7957b97b3dc562e
Gabriel Morandim <augustomorandim@gmail.com>
2020-03-01 15:08:00 -0300

    updates for web application

--- src/app/controllers/auth.js
+++ src/app/controllers/auth.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup'
 
 import User from '../models/user';
+import File from '../models/file';
 import authConfig from '../../config/auth';
 
 class AuthController {
@@ -17,7 +18,14 @@ class AuthController {
       return res.status(400).json({ error: 'Validation fails' })
     }
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [{
+        model: File,
+        as: 'avatar',
+        attributes: ['id', 'path', 'url']
+      }]
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -27,10 +35,10 @@ class AuthController {
       return res.status(401).json({ error: 'pasword does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
-      user: { id, name, email },
+      user: { id, name, email, avatar, provider },
       jwt: jwt.sign({ id }, authConfig.secret, { expiresIn: authConfig.expiresIn }),
     });
   }
--- src/app/controllers/user.js
+++ src/app/controllers/user.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup'
 
 import User from '../models/user';
+import File from '../models/file';
 
 
 class UserController {
@@ -62,9 +63,19 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' })
     }
 
-    const { id, name, provider } = await user.update(req.body)
+    await user.update(req.body);
 
-    return res.json({ id, name, email, provider });
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url']
+        }
+      ]
+    })
+
+    return res.json({ id, name, email, avatar });
   }
 }
 
