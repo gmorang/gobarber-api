50474d4e3a55d9edaf35599c035159d1db11eefb
Gabriel Morandim <augustomorandim@gmail.com>
2020-03-04 17:16:23 -0300

    bugfix provider

--- src/app/controllers/schedule.js
+++ src/app/controllers/schedule.js
@@ -8,14 +8,16 @@ class ScheduleController {
   async index(req, res) {
     const checkUserProvider = await User.findOne({ where: { id: req.userId, provider: true } })
 
-    if (!checkUserProvider) res.status(401).json({ error: 'User is not provider' })
+    if (!checkUserProvider) {
+      res.status(401).json({ error: 'User is not provider' })
+    }
 
-    const { date } = req.query
-    const parseDate = parseISO(date)
+    const { date } = req.query;
+    const parseDate = parseISO(date);
 
     const appointments = await Appointment.findAll({
       where: {
-        provider: req.userId,
+        provider_id: req.userId,
         canceled_at: null,
         date: {
           [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)]
